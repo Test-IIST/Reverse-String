acc9b7ceaeffda7aec5d31387e83406a52003f2e
CS211009-AbhishekRajputAdmin <147528739+CS211009-AbhishekRajputAdmin@users.noreply.github.com>
2023-10-15 23:02:02 +0530

    Update StudentCode_test.py

--- StudentCode_test.py
+++ StudentCode_test.py
@@ -4,16 +4,16 @@ testIO = IOWrapper()
 expectedIO = IOWrapper()
 
 def test_hello():
-    expectedList = ["Hello World"]
-    testList = []
+    expectedList = ["YES","NO","YES","YES"]
+    testList = [9474,371,1634,0]
     i=0
     for expected in expectedList:
-        #testArg = testList[i]                                                                  # use these lOCs while using list object
-        #print("Testing: Test case",testArg,"Expected",expected)
+        testArg = testList[i]                                                                  # use these lOCs while using list object
+        print("Testing: Test case",testArg,"Expected",expected)
         print("Testing: Test case","Expected",expected)
         expectedIO.print(expected)
-        #assert SCOPP_tester.test(testArg ,testIO = testIO,expectedIO = expectedIO)
-        assert SCOPP_tester.test(testIO = testIO,expectedIO = expectedIO)
+        assert SCOPP_tester.test(testArg ,testIO = testIO,expectedIO = expectedIO)
+        #assert SCOPP_tester.test(testIO = testIO,expectedIO = expectedIO)
         i+=1
         print("Test case Passed")
     print("Code is correct.All Test cases passed")
